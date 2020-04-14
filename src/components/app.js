6ad0d422bca619f2fbdde150a85e0dbeaa6987a4
Heith Robbins <heith3214@gmail.com>
2020-04-14 11:07:49 -0600

    starting point

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import NavigationComponent from "./Navbar/Navbar"
+import Home from "./pages/Home"
+import Contact from "./pages/contact"
+import About from "./pages/about"
+import Auth from "./pages/auth"
+import UserProfile from "./pages/userprofile"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+            <h1>DS Dating App</h1>
+        <Router>
+          <div>
+            <NavigationComponent />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/userProfile" component={UserProfile} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
