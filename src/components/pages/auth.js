b6a65cbdef997940dcafa023e0035966b623398f
Heith Robbins <heith3214@gmail.com>
2020-04-15 12:42:28 -0600

    With working auth page

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,9 +1,25 @@
-import React from 'react'
+// import React from 'react'
+import React, { Component } from 'react';
+import Login from "./userprofile";
 
-export default function () {
-    return (
-        <div>
-            Auth
-        </div>
-    )
+import loginImg from "../../../src/image/happycatfish.jpg";
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div className="auth-page-wrapper">
+                <div
+                    className="left-column"
+                    style={{
+                        backgroundImage: `url(${loginImg})`
+                    }}
+                />
+
+                <div className="right-column">
+                    <Login />
+                    <h1>Login Component goes here...</h1>
+                </div>
+            </div>
+        );
+    }
 }
